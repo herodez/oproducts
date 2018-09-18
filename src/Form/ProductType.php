20488b1faf5c7b152c5db85d2dda4143c4ab555e
Mario Gonzalez <pgmariodavid@gmail.com>
2018-09-18 16:44:08 -0500

    feat: Create product CRUD

---
+++ src/Form/ProductType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('code', null, array('label' => 'Codigo'))
+            ->add('name', null, array('label' => 'Nombre')) 
+            ->add('description', TextareaType::class, array('label' => 'Descripción'))
+            ->add('brand', null, array('label' => 'Marca'))
+            ->add('category', null, array('label' => 'Categoría'))
+            ->add('price', NumberType::class, array('label' => 'Precio'))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
